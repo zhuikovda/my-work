c2c767ea6caea0777338524361c68749670e5dce
dima <zh_d_a@mail.ru>
2022-06-08 19:24:50 +0300

    created numberOfFilms

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
 "use strict";
-
-// console.log(5 + +"5");
-
-// let incr = 10,
-//     decr = 10;
-
-// ++incr;
-// --decr;
-
-// console.log(++incr);
-// console.log(--decr);
-
-// console.log(5%2);
-
-// const isChecked = true,
-//       isClose = false;
-
-// console.log(isChecked || isClose);
+const numberOfFilms = 5;
