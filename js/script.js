60fc3c5a3513c978ebf89faf744f74568f54dcae
dima <zh_d_a@mail.ru>
2022-06-09 16:41:24 +0300

    added questions & answers

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+const answers = [];
+
+answers[0] = prompt ('Один из последних просмотренных фильмов?', '');
+answers[1] = prompt ('На сколько оцените его?', '');
+answers[2] = prompt ('Один из последних просмотренных фильмов?', '');
+answers[3] = prompt ('На сколько оцените его?', '');
+
+const filmOne = answers[0];
+const gradeOne = answers[1];
+const filmTwo = answers[2];
+const gradeTwo = answers[3];
+
+const movies = {
+    [filmOne]: gradeOne,
+    [filmTwo]: gradeTwo
+};
+
+// console.log(movies);
