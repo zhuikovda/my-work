9e72819d33c9de0b8edf877fe0598d2cd7fd7255
dima <zh_d_a@mail.ru>
2022-06-30 14:32:15 +0300

    Практика 29 ч.3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 "use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start()  {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -18,60 +28,54 @@ const personalMovieDB = {
 
 
 
-for (let i = 0; i < 2; i++) {
-    const filmOne = prompt ('Один из последних просмотренных фильмов?', ''),
-          gradeOne = prompt ('На сколько оцените его?', '');
 
-    if (filmOne != null && gradeOne != null && filmOne != '' && gradeOne != '' && filmOne.length < 50) {
-        personalMovieDB.movies[filmOne] = gradeOne;
-        console.log('done');
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const filmOne = prompt ('Один из последних просмотренных фильмов?', ''),
+              gradeOne = prompt ('На сколько оцените его?', '');
+    
+        if (filmOne != null && gradeOne != null && filmOne != '' && gradeOne != '' && filmOne.length < 50) {
+            personalMovieDB.movies[filmOne] = gradeOne;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }    
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+        alert("Просмотренно довольно мало фильмов");
+    } else if  ( personalMovieDB.count >= 10  && personalMovieDB.count < 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        alert("Вы киноман");
     } else {
-        console.log('error');
-        i--;
-    }    
+        alert("Произошла ошибка");
+    }
 }
 
-// Задание 24 урока первое решение
-// let i = 0;
-// while (i < 2) {
-//     const filmOne = prompt ('Один из последних просмотренных фильмов?', ''),
-//           gradeOne = prompt ('На сколько оцените его?', '');          
-
-//     if (filmOne != null && gradeOne != null && filmOne != '' && gradeOne != '' && filmOne.length < 50) {
-//         personalMovieDB.movies[filmOne] = gradeOne;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-//     i++;   
-// }
-
-// Второе решение
-// do {
-//     const filmOne = prompt ('Один из последних просмотренных фильмов?', ''),
-//           gradeOne = prompt ('На сколько оцените его?', '');          
-
-//     if (filmOne != null && gradeOne != null && filmOne != '' && gradeOne != '' && filmOne.length < 50) {
-//         personalMovieDB.movies[filmOne] = gradeOne;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-//     i++;
-// }
-// while (i < 2);
+detectPersonalLevel();
 
 
-if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
-    alert("Просмотренно довольно мало фильмов");
-} else if  ( personalMovieDB.count >= 10  && personalMovieDB.count < 30) {
-    alert("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
+// Задание 29.2.
+function showMyDB(hiden) {
+    if (!hiden) {
+        console.log(personalMovieDB);
+    }
 }
 
-console.log(personalMovieDB);
+showMyDB (personalMovieDB.privat);
+
+// Задание 29.3.
+function writeYourGeners() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);      
+    }
+}
+
+writeYourGeners();
